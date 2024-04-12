91d62c29c7d0b8cbe175833a3fec7a5d568711cf
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2024-04-13 00:18:00 +0530

    Add component ViewAllJobs.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import HomeCards from "./components/HomeCards";
 import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs";
 
 const App = () => {
   return (
@@ -10,17 +11,7 @@ const App = () => {
       <Hero />
       <HomeCards />
       <JobListings />
-
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >
-          View All Jobs
-        </a>
-      </section>
-
-      <script src="js/main.js"></script>
+      <ViewAllJobs />
     </>
   );
 };
