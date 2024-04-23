01e27b523ff11b29932a3341aaf05b25bc11da6a
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2024-04-23 20:50:27 +0530

    Add not found page

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import { FaMapMarker } from "react-icons/fa";
 import PropTypes from "prop-types";
 
@@ -37,12 +38,12 @@ const JobListing = ({ job }) => {
             <FaMapMarker className="inline text-lg mb-1 mr-1" />
             {job.location}
           </div>
-          <a
-            href={`/jobs/${job.id}`}
+          <Link
+            to={`/jobs/${job.id}`}
             className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
           >
             Read More
-          </a>
+          </Link>
         </div>
       </div>
     </div>
